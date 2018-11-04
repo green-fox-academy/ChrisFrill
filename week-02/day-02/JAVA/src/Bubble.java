714aa8088b42d020dc694221d17a8842e4e86c13
Krisztián Fodor <fkrisztian93@gmail.com>
2018-11-04 13:06:44 +0100

    Updated Bubble.

--- week-02/day-02/JAVA/src/Bubble.java
+++ week-02/day-02/JAVA/src/Bubble.java
@@ -1,22 +1,20 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Bubble {
     public static void main(String[] args) {
-
-        System.out.println(bubble(new int[]{34, 12, 24, 9, 5}));
 
         //  Create a function that takes a list of numbers as parameter
         //  Returns a list where the elements are sorted in ascending numerical order
         //  Make a second boolean parameter, if it's `true` sort that list descending
 
         //  Example:
-
+        bubble(new int[]{34, 12, 24, 9, 5});
         //  should print [5, 9, 12, 24, 34]
-        System.out.println(advancedBubble(new int[]{34, 12, 24, 9, 5}, true));
+        advancedBubble(new int[]{34, 12, 24, 9, 5}, true);
         //  should print [34, 24, 12, 9, 5]
     }
-
 
     public static int[] bubble(int[] array) {
         int temp;
@@ -35,13 +33,21 @@ public class Bubble {
 
     public static int[] advancedBubble(int[] array, boolean ascOrDesc) {
         int temp;
-        do {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Would you like to sort the list descending? (false: ascending, true: descending) ");
-            ascOrDesc = scanner.nextBoolean();
-        } while (ascOrDesc != true || ascOrDesc != false );
 
-        if(ascOrDesc){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to sort the list descending? (false: ascending, true: descending) ");
+
+        do {
+            try {
+                ascOrDesc = scanner.nextBoolean();
+                break;
+            } catch (InputMismatchException ignore) {
+                System.out.println("Please only enter true or false.");
+                scanner.next();
+            }
+        } while (true);
+
+        if (ascOrDesc) {
             for (int i = 1; i < array.length; i++) {
                 for (int j = i; j > 0; j--) {
                     if (array[j] > array[j - 1]) {
