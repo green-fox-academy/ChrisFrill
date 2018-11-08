16abfa80fa689cc6b47ccd4dd7a429da4e7b8d57
Krisztián Fodor <fkrisztian93@gmail.com>
2018-11-08 17:41:43 +0100

    Updated Triangles exercise.

--- week-03/day-05/src/Triangles.java
+++ week-03/day-05/src/Triangles.java
@@ -1,15 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
 
+import static java.awt.image.ImageObserver.WIDTH;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import static javax.swing.JFrame.setDefaultLookAndFeelDecorated;
 
 public class Triangles {
 
     public static void mainDraw(Graphics graphics) {
 
-        // Not perfect, have to work on it.
 
-        int rows = 22;
+        int rows = 50;
+        int halfCanvasWidth = WIDTH / 2;
+        int baseWidth = WIDTH / rows;
+        int halfBaseWidth = WIDTH / rows / 2;
+        int triangleHeight = HEIGHT / rows;
+        int xOne = 0;
+        int xTwo = 0;
+        int xThree = 0;
+        int x[] = {xOne, xTwo, xThree};
+        int yOne = 0;
+        int yTwoAndThree = 0;
+        int y[] = {yOne, yTwoAndThree, yTwoAndThree};
+        int nPoints = 3;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                xOne = halfCanvasWidth - (i * halfBaseWidth) + (j * baseWidth);
+                xTwo = (halfCanvasWidth + halfBaseWidth) - (i * halfBaseWidth) + (j * baseWidth);
+                xThree = (halfCanvasWidth - halfBaseWidth) - (i * halfBaseWidth) + (j * baseWidth);
+                x[0] = xOne;
+                x[1] = xTwo;
+                x[2] = xThree;
+                yOne = (i * triangleHeight);
+                yTwoAndThree = ((i + 1) * triangleHeight);
+                y[0] = yOne;
+                y[1] = yTwoAndThree;
+                y[2] = yTwoAndThree;
+                graphics.drawPolygon(x, y, nPoints);
+            }
+        }
+    }
+
+    // Not perfect, have to work on it.
+
+        /*int rows = 22;
         int halfCanvasWidth = WIDTH / 2;
         int halfBaseWidth = WIDTH / rows / 2;
         int triangleHeight = WIDTH / rows;
@@ -18,32 +53,26 @@ public class Triangles {
             graphics.drawLine(halfCanvasWidth + (i * halfBaseWidth), triangleHeight * i, i * halfBaseWidth * 2, WIDTH);
             graphics.drawLine(halfCanvasWidth - (i * halfBaseWidth), i * triangleHeight, halfCanvasWidth + (i * halfBaseWidth), i * triangleHeight);
             graphics.drawLine(halfCanvasWidth - (i * halfBaseWidth), triangleHeight * i, WIDTH - (i * halfBaseWidth * 2), WIDTH);
-        }
+        }*/
 
-        /* another version which could work in the future
-        int rows = 10;
-        int halfCanvasWidth = WIDTH / 2;
-        int halfBaseWidth = WIDTH / rows / 2;
-        int triangleHeight = WIDTH / rows;
-        for (int i = 0; i < rows + 1; i++) {
+//         another version which could work in the future
+
+        /*for (int i = 0; i < rows + 1; i++) {
             for (int j = 0; j < 1 + i * 2; j++) {
                 if (j % 2 != 0) {
                     if (i > 0) {
-                        graphics.drawLine(( halfBaseWidth + (halfCanvasWidth) - (j * (halfBaseWidth))), (i * triangleHeight), ( halfBaseWidth + halfCanvasWidth + (halfBaseWidth) - (j * (halfBaseWidth))), ((i * triangleHeight) + (triangleHeight)));
-                        graphics.drawLine(( halfBaseWidth + (halfCanvasWidth) - (j * (halfBaseWidth))), (i * triangleHeight), ( halfBaseWidth + halfCanvasWidth - (halfBaseWidth) - (j * (halfBaseWidth))), ((i * triangleHeight) + (triangleHeight)));
-                        graphics.drawLine((halfBaseWidth + halfCanvasWidth - (halfBaseWidth) - (j * (halfBaseWidth))), ((1 + i) * triangleHeight), (halfBaseWidth + halfCanvasWidth + (halfBaseWidth) - (j * (halfBaseWidth))), ((1 + i) * triangleHeight));
+                        graphics.drawLine(( halfBaseWidth + (halfCanvasWidth) - (j * (halfBaseWidth)) + (i* halfBaseWidth)), (i * triangleHeight), ( halfBaseWidth + halfCanvasWidth + (halfBaseWidth) - (j * (halfBaseWidth)) + (i* halfBaseWidth)), ((i * triangleHeight) + (triangleHeight)));
+                        graphics.drawLine(( halfBaseWidth + (halfCanvasWidth) - (j * (halfBaseWidth)) + (i* halfBaseWidth)), (i * triangleHeight), ( halfBaseWidth + halfCanvasWidth - (halfBaseWidth) - (j * (halfBaseWidth)) + (i* halfBaseWidth)), ((i * triangleHeight) + (triangleHeight)));
+                        graphics.drawLine((halfBaseWidth + halfCanvasWidth - (halfBaseWidth) - (j * (halfBaseWidth)) + (i* halfBaseWidth)), ((1 + i) * triangleHeight), (halfBaseWidth + halfCanvasWidth + (halfBaseWidth) - (j * (halfBaseWidth)) + (i* halfBaseWidth)), ((1 + i) * triangleHeight));
                     } else {
                         graphics.drawLine(((halfCanvasWidth) - (j * (halfBaseWidth))), (i * triangleHeight), (halfCanvasWidth + (halfBaseWidth) - (j * (halfBaseWidth))), ((i * triangleHeight) + (triangleHeight)));
                         graphics.drawLine(((halfCanvasWidth) - (j * (halfBaseWidth))), (i * triangleHeight), (halfCanvasWidth - (halfBaseWidth) - (j * (halfBaseWidth))), ((i * triangleHeight) + (triangleHeight)));
                         graphics.drawLine((halfCanvasWidth - (halfBaseWidth) - (j * (halfBaseWidth))), ((1 + i) * triangleHeight), (halfCanvasWidth + (halfBaseWidth) - (j * (halfBaseWidth))), ((1 + i) * triangleHeight));
-
                     }
                 }
             }
         }*/
 
-
-    }
 
     // Don't touch the code below
     static int WIDTH = 900;
@@ -60,11 +89,11 @@ public class Triangles {
         jFrame.pack();
     }
 
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
+static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        mainDraw(graphics);
     }
+}
 }
