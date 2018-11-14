a91f58f32c233d3722a69bcfd967dfea68a5e2bf
Krisztián Fodor <fkrisztian93@gmail.com>
2018-11-14 09:02:44 +0100

    Added Pokemon exercise.

---
+++ week-04/day-02-03/src/pokemon/Main.java
@@ -0,0 +1,30 @@
+package pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Pokemon> pokemonOfAsh = initializePokemons();
+
+        Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
+
+        for (Pokemon pokemon : pokemonOfAsh) {
+            if (pokemon.isEffectiveAgainst(wildPokemon)) {
+                System.out.print("I choose you, " + pokemon.name + "!");
+            }
+        }
+    }
+
+    private static List<Pokemon> initializePokemons() {
+        List<Pokemon> pokemon = new ArrayList<>();
+
+        pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
+        pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
+        pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
+        pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
+        pokemon.add(new Pokemon("Kingler", "water", "fire"));
+
+        return pokemon;
+    }
+}
