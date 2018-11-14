b755e821b5175145f9576040abcfcd5462ea37d2
Krisztián Fodor <fkrisztian93@gmail.com>
2018-11-14 09:00:35 +0100

    Added Fleet exercise.

---
+++ week-04/day-02-03/src/fleet/of/things/FleetOfThings.java
@@ -0,0 +1,22 @@
+package fleet.of.things;
+
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+
+        Thing milk = new Thing("Get Milk");
+        Thing obstacles = new Thing("Remove the obstacles");
+        Thing standUp = new Thing("Stand up");
+        standUp.complete();
+        Thing eat = new Thing("Eat lunch");
+        eat.complete();
+
+        fleet.add(milk);
+        fleet.add(obstacles);
+        fleet.add(standUp);
+        fleet.add(eat);
+
+        System.out.println(fleet);
+    }
+}
