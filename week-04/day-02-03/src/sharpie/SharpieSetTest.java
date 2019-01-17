76efe395fd41d11b54f847ec5c9410bbf4b6d298
Krisztián Fodor <fkrisztian93@gmail.com>
2019-01-17 14:07:53 +0100

    Added SharpieSetTest

---
+++ week-04/day-02-03/src/sharpie/SharpieSetTest.java
@@ -0,0 +1,23 @@
+package sharpie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SharpieSetTest {
+    SharpieSet sharpieSet;
+
+    @BeforeEach
+    void setUp() {
+        sharpieSet = new SharpieSet();
+    }
+
+/*    @Test
+
+
+    @Test
+    void testCountUsable() {
+
+    }*/
+}
